0a106d67026f31222014cb594fd9e7386dacd1ce
bluesky-350 <henrychohenrycho@gmail.com>
2024-05-31 08:43:32 +0900

    Hcms_ver3.0_2024/05/31_Polymorphism

--- Hcms.java
+++ Hcms.java
@@ -1,168 +1,169 @@
-import java.io.*;
 import java.time.LocalDateTime;
 import java.time.Duration;
-import java.util.*;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class hcms2_202321811 {
+public class hcms3_202321811 { // 23 lines!!!
     public static void main(String[] args) throws IOException {
-        int count;
-        String command=null;
-        Highway highway = new Highway();
-        EnterGate enterGate;
-        ExitGate exitGate;
-        CarList cl;
-        Time time;
+        BufferedReader br = new BufferedReader(new FileReader("hcms.txt"));
+        ExitGate.setExitGate(br);
+        VehicleList.setVehicleList(br);
 
-        Scanner scan = new Scanner(new File("hcms.txt"));
-        exitGate = new ExitGate(scan.nextInt(), scan.nextInt());
-        cl = new CarList(scan.nextInt());
-        cl.setcarList(scan);
-        scan.close();
+        CurrentTime.setCurrentTime(new Time(2024, 3, 20, 21, 00));
 
-        time = new Time(2024, 3, 20, 21, 0);
-
-        Scanner in = new Scanner(System.in);
+        br = new BufferedReader(new InputStreamReader(System.in));
         while(true){
-            try{
-                command=in.next();
-            } catch(Exception e){
-                System.out.println("[error - wrong command character!]");
-                in.nextLine();
-            }
-
-            if(command.equals("t")){
-                Time temp = new Time(in.nextInt(), in.nextInt(), in.nextInt(), in.nextInt(), in.nextInt());
-                if(time.checkTimeError(temp)){
-                    time = temp;
-                }
-                else continue;
-                highway.updateCar(cl.getcarList(), time, exitGate);
-                continue;
-            }
-
-            else if(command.equals("n")){
-                int num=0;
-                try{
-                    num = in.nextInt();
-                } catch(Exception e) { System.out.println("[error - car number format is wrong!]"); in.nextLine(); }
-                count=0;
-                for(Car c: cl.getcarList()){
-                    if(c.getNumber()==num){
-                        count++;
-                        if(c.getOnway()==1||c.getOnway()==2) { System.out.println("[error - Aleardy running car!]"); in.nextLine(); continue; }
-                        enterGate = new EnterGate(time);
-                        try{
-                            enterGate.enterCar(c, in.next(), in.next(), time);
-                        } catch(Exception e) { System.out.println("[error - Point format is wrong!]"); in.nextLine(); }
-                    }
-                }
-                if(count==0) { System.out.println("[error - car number does not exist!]"); in.nextLine(); }
-                continue;
-            }
-
-            else if(command.equals("o")){
-                count=0;
-                for(Car c: cl.getcarList()){
-                    if(c.getOnway()==1||c.getOnway()==2){
-                        count++;
-                        System.out.println(count+". "+c.getNumber()+" "+c.getPosition()+"km");
-                    }
-                }
-                if(count==0) System.out.println("통행 차량이 없습니다!");
-                continue;
-            }
-
-            else if(command.equals("x")){
-                count=0;
-                for(Car c: cl.getcarList()){
-                    if(c.getOnway()==3){
-                        count++;
-                        System.out.println(count+". "+c.getNumber()+" "+c.getExitTime()+" "+c.getFee()+"원");
-                    }
-                }
-                if(count==0) System.out.println("진출한 차량이 없습니다!");
-                continue;
-            }
-
-            else if(command.equals("r")){
-                try{
-                    cl.registerCar(in.nextInt(), in.nextDouble(), in.nextDouble());
-                } catch(Exception e){
-                    System.out.println("[error - car information format is wrong!]");
-                    in.nextLine();
-                }
-                
-                continue;
-            }
-
-            else if(command.equals("q")){
-                in.close();
-                System.exit(0);
-            }
-
-            else{
-                System.out.println("[error - wrong command character!]");
-                in.nextLine();
-                continue;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            switch(st.nextToken()){
+                case "t": OrderT o1 = new OrderT(); o1.execute(st, CurrentTime.getCurrentTime()); break;
+                case "n": OrderN o2 = new OrderN(); o2.execute(st, CurrentTime.getCurrentTime()); break;
+                case "o": OrderO o3 = new OrderO(); o3.execute(st, CurrentTime.getCurrentTime()); break;
+                case "x": OrderX o4 = new OrderX(); o4.execute(st, CurrentTime.getCurrentTime()); break;
+                case "r": OrderR o5 = new OrderR(); o5.execute(st, CurrentTime.getCurrentTime()); break;
+                case "q": br.close(); System.exit(0);
+                default: OrderError o7 = new OrderError(); o7.execute(st, CurrentTime.getCurrentTime()); break;
             }
         }
-
     }
 }
 
-class CarList {
-    private int current;
-    private Car[] carList;
+interface Command {
+    public abstract void execute(StringTokenizer st, Time time);
+}
 
-    CarList(int n){
-        current=n;
-        carList = new Car[n];
+class OrderT implements Command {
+    @Override
+    public void execute(StringTokenizer st, Time time){
+        Time temp = new Time(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+        if(time.checkTimeError(temp)){
+            CurrentTime.setCurrentTime(temp);
+            for(Vehicle v: VehicleList.getVehicleList()){
+                if(v.getOnway()==3) v.setOnway(4);
+            }
+        }
+        else return;
+        Highway.updateCar(CurrentTime.getCurrentTime());
     }
+}
 
-    public Car[] getcarList(){ return carList; }
-    public void setcarList(Scanner s){
+class OrderN implements Command {
+    @Override
+    public void execute(StringTokenizer st, Time time){
+        int num=Integer.parseInt(st.nextToken());
+        for(Vehicle v: VehicleList.getVehicleList()){
+            if(v.getNumber()==num){
+                if(v.getOnway()==1||v.getOnway()==2) { System.out.println("[error - Aleardy running vehicle!]"); return; }
+                EnterGate.enterVehicle(v, st.nextToken(), st.nextToken(), time);
+                return;
+            }
+        }
+        System.out.println("[error - vehicle number does not exist!]");
+    }
+}
+
+class OrderO implements Command {
+    @Override
+    public void execute(StringTokenizer st, Time time){
+        int count=0;
+        for(Vehicle v: VehicleList.getVehicleList()){
+            if(v.getOnway()==1||v.getOnway()==2){
+                count++;
+                System.out.println(count+". "+v.o());
+            }
+        }
+        if(count==0) System.out.println("통행 차량이 없습니다!");
+    }
+}
+
+class OrderX implements Command {
+    @Override
+    public void execute(StringTokenizer st, Time time){
+        int count=0;
+        for(Vehicle v: VehicleList.getVehicleList()){
+            if(v.getOnway()==3){
+                count++;
+                System.out.println(count+". "+v.x());
+            }
+        }
+        if(count==0) System.out.println("진출한 차량이 없습니다!");
+    }
+}
+
+class OrderR implements Command {
+    @Override
+    public void execute(StringTokenizer st, Time time){
+        VehicleList.registerVehicle(st.nextToken(), Integer.parseInt(st.nextToken()), Double.parseDouble(st.nextToken()), Double.parseDouble(st.nextToken()));
+    }
+}
+
+class OrderError implements Command {
+    @Override
+    public void execute(StringTokenizer st, Time time){
+        System.out.println("[error - wrong command character!]");
+    }
+}
+
+class VehicleList {
+    private static int current;
+    private static Vehicle[] vehicleList;
+    private static Vehicle[] tempList;
+
+    public static void setVehicleList(BufferedReader br) throws IOException {
+        current = Integer.parseInt(br.readLine());
+        vehicleList = new Vehicle[current];
+        StringTokenizer st = null;
         for(int i=0;i<current;i++){
-            carList[i] = new Car(s.nextInt(), s.nextDouble(), s.nextDouble());
+            st = new StringTokenizer(br.readLine());
+            switch(st.nextToken()){
+                case "g": vehicleList[i] = new GasCar(Integer.parseInt(st.nextToken()), Double.parseDouble(st.nextToken()), Double.parseDouble(st.nextToken())); break;
+                case "b": vehicleList[i] = new Bus(Integer.parseInt(st.nextToken()), Double.parseDouble(st.nextToken()), Double.parseDouble(st.nextToken())); break;
+                case "t": vehicleList[i] = new Truck(Integer.parseInt(st.nextToken()), Double.parseDouble(st.nextToken()), Double.parseDouble(st.nextToken())); break;
+                case "h": vehicleList[i] = new HybridCar(Integer.parseInt(st.nextToken()), Double.parseDouble(st.nextToken()), Double.parseDouble(st.nextToken())); break;
+                default: break;
+            }
         }
     }
-    public void registerCar(int n,double d,double s){
-        for(Car c: carList){
-            if(c.getNumber()==n) {
-                System.out.println("[error - already registered car number!]");
+    public static Vehicle[] getVehicleList(){ return vehicleList; }
+    public static void registerVehicle(String type, int n,double d,double s){
+        for(Vehicle v: vehicleList){
+            if(v.getNumber()==n) {
+                System.out.println("[error - already registered vehicle number!]");
                 return;
             }
         }
         current++;
-        Car[] temp = new Car[current];
+        tempList = new Vehicle[current];
         for(int i=0;i<current-1;i++){
-            temp[i]=carList[i];
+            tempList[i]=vehicleList[i];
         }
-        temp[current-1]=new Car(n,d,s);
-        carList=temp;
+        if(type.equals("g")) tempList[current-1]=new GasCar(n,d,s);
+        else if(type.equals("b")) tempList[current-1]=new Bus(n,d,s);
+        else if(type.equals("t")) tempList[current-1]=new Truck(n,d,s);
+        else if(type.equals("h")) tempList[current-1]=new HybridCar(n,d,s);
+        vehicleList=tempList;
     }
 }
 
-class Car {
+abstract class Vehicle{
     private final int number;
-    private final double displacement;
     private final double speed;
     private int onway=0;
     private Time enterTime;
-    private String enterPoint;
     private int enterDistance;
     private Time exitTime;
-    private String exitPoint;
     private int exitDistance;
     private int distance;
     private int position;
     private int fee;
 
-    Car(int number, double displacement, double speed){
-        this.number=number; this.displacement=displacement; this.speed=speed;
+    Vehicle(int number, double speed){
+        this.number=number; this.speed=speed;
     }
 
     public int getNumber(){ return number; }
-    public double getDisplacement(){ return displacement; }
     public double getSpeed(){ return speed; }
     public void setOnway(int onway){
         this.onway = onway;
@@ -193,23 +194,110 @@ class Car {
         this.fee=fee;
     }
     public int getFee(){ return fee; }
+    public String o(){
+        return getType()+" "+getNumber()+" "+getPosition()+"km";
+    }
+    public String x(){
+        return getType()+" "+getNumber()+" "+getExitTime()+" "+getFee()+"원";
+    }
+
+    abstract public String getType();
+    abstract public void calcFee(double basicFee, double distanceRate);
+}
+
+class GasCar extends Vehicle {
+    private final double displacement;
+    
+    GasCar(int number, double speed, double displacement){
+        super(number,speed); this.displacement=displacement;
+    }
+
+    public double getDisplacement(){ return displacement; }
+    @Override
+    public String getType(){ return "가솔린차"; }
+    @Override
+    public void calcFee(double basicFee, double distanceRate){
+        double displacementRate;
+        if(displacement<1000) displacementRate=0.8;
+        else if(displacement<2000) displacementRate=1.0;
+        else if(displacement<3000) displacementRate=1.2;
+        else displacementRate=1.5;
+        setFee((int)((basicFee+getDistance()*distanceRate)*(getSpeed()/100)*displacementRate/10)*10);
+    }
+}
+
+class Bus extends Vehicle {
+    private final double max_passengers;
+
+    Bus(int number, double speed, double max_passengers){
+        super(number,speed); this.max_passengers=max_passengers;
+    }
+
+    public double getMax_passengers() { return max_passengers; }
+    @Override
+    public String getType(){ return "버스"; }
+    @Override
+    public void calcFee(double basicFee, double distanceRate){
+        double passengerRate;
+        if(max_passengers<12) passengerRate=0.8;
+        else if(max_passengers<20) passengerRate=1.0;
+        else if(max_passengers<30) passengerRate=1.2;
+        else passengerRate=1.5;
+        setFee((int)((basicFee+getDistance()*distanceRate)*(getSpeed()/100)*passengerRate/10)*10);
+    }
+}
+
+class Truck extends Vehicle {
+    private final double ton;
+
+    Truck(int number, double speed, double ton){
+        super(number,speed); this.ton=ton;
+    }
+
+    public double getTon(){ return ton; }
+    @Override
+    public String getType(){ return "트럭"; }
+    @Override
+    public void calcFee(double basicFee, double distanceRate){
+        double tonRate;
+        if(ton<1) tonRate=0.8;
+        else if(ton<2) tonRate=1.0;
+        else if(ton<4) tonRate=1.2;
+        else tonRate=1.5;
+        setFee((int)((basicFee+getDistance()*distanceRate)*(getSpeed()/100)*tonRate/10)*10);
+    }
+}
+
+class HybridCar extends Vehicle {
+    private final double displacement;
+    
+    HybridCar(int number, double speed, double displacement){
+        super(number,speed); this.displacement=displacement;
+    }
+
+    public double getDisplacement(){ return displacement; }
+    @Override
+    public String getType(){ return "하이브리드차"; }
+    @Override
+    public void calcFee(double basicFee, double distanceRate){
+        double displacementRate;
+        if(displacement<1000) displacementRate=0.8;
+        else if(displacement<2000) displacementRate=1.0;
+        else if(displacement<3000) displacementRate=1.2;
+        else displacementRate=1.5;
+        setFee((int)((basicFee+getDistance()*distanceRate)*(getSpeed()/100)*displacementRate/2/10)*10);
+    }
 }
 
 class EnterGate {
-    private Time enterTime;
 
-    EnterGate(Time t){
-        enterTime=t;
-    }
-
-    public Time getEnterTime(){ return enterTime; }
-    public boolean checkPoint(String p1, String p2){
+    public static boolean checkPoint(String p1, String p2){
         if(p1.equals(p2)) { System.out.println("[error - entry point and exit point must be different!]"); return false; }
         if((p1.equals("서울")||p1.equals("수원")||p1.equals("대전")||p1.equals("대구")||p1.equals("부산"))==false) { System.out.println("[error - entry point should be included in list!]"); return false; }
         if((p2.equals("서울")||p2.equals("수원")||p2.equals("대전")||p2.equals("대구")||p2.equals("부산"))==false) { System.out.println("[error - exit point should be included in list!]"); return false; }
         return true;
     }
-    public void enterCar(Car c,String p1,String p2,Time t){
+    public static void enterVehicle(Vehicle v,String p1,String p2,Time t){
         if(checkPoint(p1,p2)){
             int d1, d2;
             switch(p1){
@@ -227,55 +315,51 @@ class EnterGate {
                 default: d2=400; break;
             }
             if(d2-d1>0){
-                c.setDistance(d2-d1);
-                c.setOnway(1);
+                v.setDistance(d2-d1);
+                v.setOnway(1);
             }
             else{
-                c.setDistance(d1-d2);
-                c.setOnway(2);
+                v.setDistance(d1-d2);
+                v.setOnway(2);
             }
-            c.setPoint(d1,d2);
-            c.setPosition(d1);
-            c.setEnterTime(enterTime);
-            c.setExitTime(t.plusTime((int)(c.getDistance()/c.getSpeed()*60)));            
+            v.setPoint(d1,d2);
+            v.setPosition(d1);
+            v.setEnterTime(t);
+            v.setExitTime(t.plusTime((int)(v.getDistance()/v.getSpeed()*60)));     
         }
     }
-
 }
 
 class ExitGate {
     private static int basicFee;
     private static int distanceRate;
-
-    ExitGate(int basicFee, int distanceRate) {
-        this.basicFee=basicFee; this.distanceRate=distanceRate;
+    
+    public static void setExitGate(BufferedReader br) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        ExitGate.basicFee=Integer.parseInt(st.nextToken()); ExitGate.distanceRate=Integer.parseInt(st.nextToken());
     }
-
-    public int calcFee(Car c){
-        return (int)((basicFee+c.getDistance()*distanceRate)*(c.getSpeed()/100)*(c.getDisplacement()/2000)/10)*10;
-    }
-    public void exitCar(Car c){
-        c.setOnway(3);
-        c.setFee(calcFee(c));
+    public static void exitVehicle(Vehicle v){
+        v.setOnway(3);
+        v.calcFee(basicFee,distanceRate);     
     }
 }
 
 class Highway {
 
-    public void updateCar(Car[] cl, Time t, ExitGate e){
-        for(Car c: cl){
-            if(c.getOnway()==1||c.getOnway()==2){
-                int migration = (int)(t.getInterval(c.getEnterTime(), t)*c.getSpeed()/60.0);
-                if(migration<c.getDistance()){
-                    if(c.getOnway()==1){
-                        c.setPosition(c.getEnterDistatnce()+migration);
+    public static void updateCar(Time t){
+        for(Vehicle v: VehicleList.getVehicleList()){
+            if(v.getOnway()==1||v.getOnway()==2){
+                int migration = (int)(t.getInterval(v.getEnterTime(), t)*v.getSpeed()/60.0);
+                if(migration<v.getDistance()){
+                    if(v.getOnway()==1){
+                        v.setPosition(v.getEnterDistatnce()+migration);
                     }
                     else{
-                        c.setPosition(c.getEnterDistatnce()-migration);
+                        v.setPosition(v.getEnterDistatnce()-migration);
                     }
                 }
                 else{
-                    e.exitCar(c);
+                    ExitGate.exitVehicle(v);
                 }
             }
         }
@@ -338,5 +422,13 @@ class Time {
     public String toString(){
         return String.format("%02d/%02d/%02d-%02d:%02d", getYear(), getMonth(), getDate(), getHour(), getMinute());
     }
-        
+}
+
+class CurrentTime {
+    private static Time time;
+
+    public static void setCurrentTime(Time time){
+        CurrentTime.time = time;
+    }
+    public static Time getCurrentTime(){ return time; }
 }
